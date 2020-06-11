be06c55a4979abc4acfd1a70b874dbc3173f7c08
Caterina <ccnn1010@gmail.com>
2020-06-11 23:38:08 +1000

    work on all pages

---
+++ script.js
@@ -0,0 +1,13 @@
+/* Translation button in Navbar */
+
+const translateToEnglish = () => {
+    $(".english").css("display", "block")
+    $(".spanish").css("display", "none")
+  }
+  
+  
+  const translateToSpanish = () => {
+    $(".english").css("display", "none")
+    $(".spanish").css("display", "block")
+  }
+  
